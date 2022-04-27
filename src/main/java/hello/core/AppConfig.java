71daf74708126b77a7daeb7bb4bc2c102669b235
taekgyu <taekgyu0602@gmail.com>
2022-04-28 00:09:30 +0900

    스프링으로 변환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,16 +9,21 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration  // AppConfig에 설정을 구성한다는 뜻의 @Configuration 을 붙여준다.
 public class AppConfig {
 
     // 역할
+    @Bean   // 각 메서드에  @Bean 을 붙여준다. 이렇게 하면 스프링 컨테이너에 스프링 빈으로 등록한다.
     public MemberService memberService(){
         // 이렇게하면 생정자 주입을 통해서 DIP원칙을 위반하지 않음
         return new MemberServiceImpl(memberRepository());
     }
 
     // 역할
+    @Bean
     public OrderService orderService() {
         // 이렇게하면 생정자 주입을 통해서 DIP원칙을 위반하지 않음
         return new OrderServiceImpl(
@@ -26,11 +31,13 @@ public class AppConfig {
     }
 
     // 구현
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 구현
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
