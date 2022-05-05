f16322ef11bcefed961bc4915a4abbb27815d74e
taekgyu <taekgyu0602@gmail.com>
2022-05-05 23:03:37 +0900

    필터로 컴포넌트 스캔대상 적용 테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // 자동으로 클래스 패스를 뒤져서  @Component가 붙었으면 스프링 빈에 등록해준다.
+        //basePackages = "hello.core.member",
+        //basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes =
                 Configuration.class))
 public class AutoAppConfig {
